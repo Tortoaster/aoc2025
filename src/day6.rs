192f1407cf01b6e993614ee980785b43e9e60374
Rick van der Wal <rick@tortoaster.com>
2025-12-13 10:35:36 +0100

    Day 6 part two

--- src/day6.rs
+++ src/day6.rs
@@ -1,26 +1,63 @@
-use std::str::FromStr;
+use std::{iter, str::FromStr};
 
 use anyhow::{Error, anyhow};
+use itertools::Itertools;
 
 pub fn solve_a(input: &str) -> u64 {
-    parse(input).map(Expr::eval).sum()
-}
-
-pub fn solve_b(input: &str) -> u64 {
-    0
-}
-
-fn parse(input: &str) -> impl Iterator<Item = Expr> {
     let matrix: Vec<Vec<_>> = input
         .lines()
         .map(|line| line.split_whitespace().collect())
         .collect();
+
+    transpose(matrix)
+        .into_iter()
+        .map(|parts| Expr::from_parts(&parts))
+        .map(Expr::eval)
+        .sum()
+}
+
+pub fn solve_b(input: &str) -> u64 {
+    let ops_line = input.lines().last().unwrap();
+    let matrix = input
+        .lines()
+        .take(4)
+        .chain(iter::once(
+            &*iter::repeat_n(' ', ops_line.len()).collect::<String>(),
+        ))
+        .chain(iter::once(ops_line))
+        .map(|line| line.chars().collect())
+        .collect();
+
+    transpose(matrix)
+        .into_iter()
+        .map(|line| line.into_iter().collect::<String>())
+        .batching(|it| {
+            let mut op = None;
+            let mut nums = Vec::new();
+            while let Some((num, o)) = batching_line(it) {
+                nums.push(num);
+                op = op.or(o);
+            }
+            Some(Expr { nums, op: op? })
+        })
+        .map(Expr::eval)
+        .sum()
+}
+
+fn transpose<T: Copy>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> {
     let rows = matrix.len();
     let cols = matrix[0].len();
-    let transposed: Vec<Vec<_>> = (0..cols)
+    (0..cols)
         .map(|col| (0..rows).map(|row| matrix[row][col]).collect())
-        .collect();
-    transposed.into_iter().map(|parts| Expr::from_parts(&parts))
+        .collect()
+}
+
+fn batching_line(it: &mut impl Iterator<Item = String>) -> Option<(u64, Option<Op>)> {
+    let line = it.next()?;
+    let mut parts = line.split_whitespace();
+    let num = parts.next()?.parse().unwrap();
+    let op = parts.next().map(Op::from_str).transpose().unwrap();
+    Some((num, op))
 }
 
 struct Expr {
